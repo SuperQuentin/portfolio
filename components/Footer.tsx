7752521976b406e4fc4f2a7f4f6c0bbe0e645e72
SuperQuentin <quentin.aellen@antistatique.net>
2022-01-26 00:59:17 +0100

    Add margin top on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,9 @@ import clsx from "clsx";
 const Footer = () => {
   return (
     <footer
-      className={clsx("relative h-12 bg-slate-800 bottom-0 left-0 right-0")}
+      className={clsx(
+        "relative h-12 mt-24 bg-slate-800 bottom-0 left-0 right-0"
+      )}
     >
       <div className={clsx("flex justify-center h-full items-center")}>
         <h3 className={clsx("text-sm text-slate-200")}>
