77f8c64a4a57940a4bef77f14e3214a5862aa6f4
SuperQuentin <quentin.aellen@antistatique.net>
2022-01-25 21:13:00 +0100

    Fix to the bottom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,17 @@
-import clsx from "clsx"
+import clsx from "clsx";
 
-const Footer = () =>{
-    return (
-        <footer className={clsx("h-12 bg-slate-800")}>
-            <div className={clsx("flex justify-center h-full items-center")}>
-                <h3 className={clsx("text-sm text-slate-200")}>© Quentin Aellen 2020</h3>
-            </div>
-        </footer>
-    )
+const Footer = () => {
+  return (
+    <footer
+      className={clsx("relative h-12 bg-slate-800 bottom-0 left-0 right-0")}
+    >
+      <div className={clsx("flex justify-center h-full items-center")}>
+        <h3 className={clsx("text-sm text-slate-200")}>
+          © Quentin Aellen 2020
+        </h3>
+      </div>
+    </footer>
+  );
+};
 
-}
-
-export default Footer
+export default Footer;
--- components/layouts/DefaultLayout.tsx
+++ components/layouts/DefaultLayout.tsx
@@ -1,26 +1,24 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import Head from "next/head";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import clsx from "clsx";
 
 type DefaultLayoutProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
 // eslint-disable-next-line react/display-name
-export default function DefaultLayout ({children} : DefaultLayoutProps) {
-    return (
-        <>
-            <Head>
-                <title>Portfolio</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-            <Navbar />
-            <main className={clsx('w-full')}>
-                {children}
-            </main>
-            <Footer />
-        </>
-    )
+export default function DefaultLayout({ children }: DefaultLayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Navbar />
+      <main className={clsx("w-full min-h-screen")}>{children}</main>
+      <Footer />
+    </>
+  );
 }
