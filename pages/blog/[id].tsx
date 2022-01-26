98e44e29d98dd7b7fd8fe60dea9b3c8f5d7c5792
SuperQuentin <quentin.aellen@antistatique.net>
2022-01-26 01:47:58 +0100

    Add pretty display blog article

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,12 +1,36 @@
 import DefaultLayout from "../../components/layouts/DefaultLayout";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import { ReactElement } from "react";
+import clsx from "clsx";
 
-const Post = () => {
+const { Client } = require("@notionhq/client");
+
+const Post = ({ content }: any) => {
   const router = useRouter();
   const { id } = router.query; // get id from query string
 
-  return <h2>Post : {id}</h2>;
+  return (
+    <div className={clsx("relative w-full h-screen -mt-20")}>
+      <div className={clsx("max-w-7xl m-auto h-full")}>
+        <div className={clsx("flex justify-center items-center h-full")}>
+          <h2 className={clsx("text-7xl text-white font-bold")}>Post : {id}</h2>
+          ;
+        </div>
+      </div>
+      <div
+        className={clsx("absolute w-full h-full object-cover inset-0 z-[-10]")}
+      >
+        <Image
+          layout={"fill"}
+          objectFit={"cover"}
+          src={"/marcel-strauss-7DigijJqGlo-unsplash.jpg"}
+          alt="background-image"
+          priority={true}
+        />
+      </div>
+    </div>
+  );
 };
 
 Post.getLayout = function getLayout(page: ReactElement) {
